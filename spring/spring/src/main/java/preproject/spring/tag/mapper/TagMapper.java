18763a23dc1ea462a54b04761149337406f98b0f
kimsumin <qt5860@naver.com>
2023-08-16 10:54:56 +0900

    태그 매퍼 수정

--- spring/spring/src/main/java/preproject/spring/tag/mapper/TagMapper.java
+++ spring/spring/src/main/java/preproject/spring/tag/mapper/TagMapper.java
@@ -3,20 +3,10 @@ package preproject.spring.tag.mapper;
 import org.mapstruct.Mapper;
 
 import preproject.spring.tag.dto.TagPostDto;
-import preproject.spring.tag.dto.TagResponseDto;
 import preproject.spring.tag.entity.Tag;
 
 @Mapper(componentModel = "spring")
 public interface TagMapper {
     Tag tagPostDtoToQuestion(TagPostDto tagPostDto);
-
-    default TagResponseDto tagToTagResponseDto(Tag tag){
-
-        TagResponseDto tagResponseDto = new TagResponseDto();
-        tagResponseDto.setTagId(tag.getTagId());
-        tagResponseDto.setTagTitle(tag.getTagTitle());
-
-        return tagResponseDto;
-    }
 
 }
