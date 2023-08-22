c7426b7173cd294481f4885823aaa6cd6ece9b30
Nydaro <qhang@naver.com>
2023-08-22 13:40:36 +0900

    + Spring Security CORS정책 적용

--- spring/spring/src/main/java/preproject/spring/Security/SecurityConfig.java
+++ spring/spring/src/main/java/preproject/spring/Security/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -20,11 +25,12 @@ public class SecurityConfig {
                 .authorizeRequests()
                 .anyRequest().permitAll() //요청 전부 허용
                 .and()
+                .csrf().disable()
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
                 .headers().frameOptions().disable()// h2 콘솔 db 내용 허용
                 .and()
-                .httpBasic()//postman 요청 허용
-                .and()
-                .csrf().disable();
+                .httpBasic();//postman 요청 허용
 
         return http.build();
 
@@ -32,5 +38,19 @@ public class SecurityConfig {
     @Bean // 비밀번호 인코딩을 위함
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://43.200.41.110:8080","http://localhost:8080","http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","PATCH","DELETE","POST","OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization","Refresh"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
     }
 }
